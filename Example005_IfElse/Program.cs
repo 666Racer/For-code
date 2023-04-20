86b5ec05795752c9d623ad05ea4e18d20bcb5c0e
Misha <misha.hanzhin@gmail.com>
2023-04-20 20:39:48 +0500

    Add Example 005 IfElse Project

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введитя имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лера")
+{
+    Console.WriteLine("Ура, это же ЛЕРА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
